b5b924016eeee4a730c532862ae6a83299b56b3d
Arjun M <78254828+Arjun-M-code@users.noreply.github.com>
2025-03-02 20:23:01 +0530

    add for testing  purpose

---
+++ sample_bot.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, Response
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages.text_message import TextMessage
+from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+from viberbot.api.viber_requests import ViberUnsubscribedRequest
+
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+app = Flask(__name__)
+viber = Api(BotConfiguration(
+        name='RAN TI ROBOT',
+        avatar='',
+        auth_token='50dbc10ac127e495-7681f4e91f842ab7-52399b989d61ea36',))
+
+@app.route('/', methods=['POST'])
+def incoming():
+	logger.debug("received request. post data: {0}".format(request.get_data()))
+
+	viber_request = viber.parse_request(request.get_data().decode('utf8'))
+
+	if isinstance(viber_request, ViberMessageRequest):
+		message = viber_request.message
+		viber.send_messages(viber_request.sender.id, [
+			message
+		])
+	elif isinstance(viber_request, ViberConversationStartedRequest) \
+			or isinstance(viber_request, ViberSubscribedRequest) \
+			or isinstance(viber_request, ViberUnsubscribedRequest):
+		viber.send_messages(viber_request.sender.id, [
+			TextMessage(None, None, viber_request.get_event_type())
+		])
+	elif isinstance(viber_request, ViberFailedRequest):
+		logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+
+	return Response(status=200)
+
+
+if __name__ == "__main__":
+	app.run()
